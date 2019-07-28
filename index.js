e4f5705de6618da31ee85039efac0f2b69dce1b2
unknown <khaliduni11@gmail.com>
2019-07-28 08:16:40 +0300

    fixing issue

--- index.js
+++ index.js
@@ -6,10 +6,11 @@ const bodyParser = require("body-parser");
 const Port = process.env.PORT || 3001;
 const Auth = require("./routes/auth");
 const Post = require("./routes/post");
+const path = require("path");
 
 
 app.use(cors());
-app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
 app.use("/user/auth", Auth);
@@ -17,6 +18,9 @@ app.use("/user/auth", Auth);
 app.use("/post", Post);
 
 
+app.get('*', function (request, response) {
+    response.sendFile(path.resolve(__dirname, '../Reactjs/build', 'index.html'));
+});
 
 app.use(function (req, res, next) {
     let err = new Error("Not Found");
@@ -24,7 +28,7 @@ app.use(function (req, res, next) {
     next(err);
 });
 
-app.use(function(error, request, respone, next){
+app.use(function (error, request, respone, next) {
     return respone.status(error.status || 500).json({
         message: error.message || "Oops something went wrong"
     })
